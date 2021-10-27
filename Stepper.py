260cd7d47ecad00040ad4faa702c7438b6e295a0
TrevConk <trevorconklin@gmail.com>
2021-10-27 16:20:18 -0400

    initial commit

---
+++ Stepper.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
+for pin in pins:
+  GPIO.setup(pin, GPIO.OUT, initial=0)
+
+# Define the pin sequence for counter-clockwise motion, noting that
+# two adjacent phases must be actuated together before stepping to
+# a new phase so that the rotor is pulled in the right direction:
+ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+        
+
+
+state = 0
+
+def delay_us(tus): # use microseconds to improve time resolution
+  endTime = time.time() + float(tus)/ float(1E6)
+  while time.time() < endTime:
+    pass
+
+def halfstep(dir):
+    #direction +/- one + -> ccw - -> cw
+    state += dir
+    if state > 7:
+        state = 0
+    elif state < 0:
+        state = 7
+    for pin in range(4):    # 4 pins that need to be energized
+        GPIO.output(pins[pin], dir[halfstep][pin])
+
+def moveSteps(steps, dir):
+    for step in steps:
+        halfstep(dir)
+
+
+try:
+  loop(cw)
+  loop(ccw)
+except:
+  pass
+GPIO.cleanup() 
---
+++ stepper_control.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python37all
+import cgi
+import cgitb
+import json
+
+cgitb.enable()
+data = cgi.FieldStorage()
+
+if value in data.getvalue('angleText'):
+    angle = value
+else:
+    angle = data.getValue('angleRange')
+
+selection = data.getvalue('submit')
+
+Brightness = data.getvalue('BrightVal')
+dataDump = {'Angle':angle,'Selection':selection}
+with open('stepperControlDump.txt', 'w') as f:
+  json.dump(dataDump, f)
+
+if selection == 'zero':
+    angle = 0
+
+print('Content-type: text/html\n\n')
+print('''
+    <html>
+    <form action="/cgi-bin/stepper_control.py" method="POST">''')
+print('<b>Use Slider to Input Angle. Current angle = %s</b>' %angle)
+print('<input type="range" name="angleRange" min="0" max="180" value="%s"><br>' % angle)
+print('''
+    <input type="text" name="angleText"><br>
+    <input type="submit" name="submit" value="Zero"><br>
+    <input type="submit" name="submit" value="Submit"><br>
+    </input>
+</form>
+</html>''')
